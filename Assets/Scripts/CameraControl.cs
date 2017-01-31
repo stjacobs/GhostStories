3faafb184fc93747c0130ee9b7390c5f2534aee4
Mikael <mialmg@kth.se>
2017-01-31 22:39:55 +0100

    movement test

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	private float height = 5; // height from center
+	private float leftWidth = 8.9f;
+	private float rightWidth = 4;
+
+	private void moveUp() {
+		transform.position += new Vector3(0, 10);
+	}
+
+	private void moveDown() {
+		transform.position -= new Vector3(0, 10);
+	}
+
+	private void moveLeft() {
+		transform.position -= new Vector3(12.9f, 0);
+	}
+
+	private void moveRight() {
+		transform.position += new Vector3(12.9f, 0);
+	}
+
+	public void updatePosition(Control player) {
+		if (player.transform.position.x < transform.position.x - leftWidth) {
+			print ("left");
+			moveLeft();
+		} else if (player.transform.position.x > transform.position.x + rightWidth) {
+			moveRight ();
+			print ("right");
+		} else if (player.transform.position.y < transform.position.y - height) {
+			moveDown ();
+			print ("down");
+		} else if (player.transform.position.y > transform.position.y + height) {
+			moveUp ();
+			print ("up");
+		}
+	}
+
+	public void LateUpdate() {
+	}
+
+}
---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+	public float speed;
+
+	private float xDelta = 0;
+	private float yDelta = 0;
+	private CameraControl cameraControl;
+
+	// Use this for initialization
+	void Start () {
+		cameraControl = GameObject.Find("Main Camera").GetComponent<CameraControl> ();
+	}
+
+	void Update() {
+		xDelta += Input.GetAxisRaw ("Horizontal");
+		yDelta += Input.GetAxisRaw ("Vertical");
+	}
+
+	void FixedUpdate () {
+		transform.position = transform.position + new Vector3 (xDelta / 10, yDelta / 10);
+		xDelta = 0;
+		yDelta = 0;
+		cameraControl.updatePosition (this);
+	}
+
+}
